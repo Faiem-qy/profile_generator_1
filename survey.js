ff7b1bb0a8cfa5d9366f764024265c0a13c7ab7c
Faiem-qy <f.quanyoung@gmail.com>
2023-08-09 19:09:54 -0400

    small change

--- survey.js
+++ survey.js
@@ -3,7 +3,7 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-});
+}); 
 
 rl.question('What do you think of Node.js? ', (answer) => {
 
